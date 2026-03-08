973a08f92a8d261eb2613c0177dc1e9c27c7b3fe
2464480 <2464480@LTIN656414>
2026-03-08 10:21:42 +0530

    fixed idearequest validators

--- Models/DTO/Idea/IdeaRequestDto.cs
+++ Models/DTO/Idea/IdeaRequestDto.cs
@@ -5,11 +5,11 @@ namespace backend_trial.Models.DTO.Idea
     public class IdeaRequestDto
     {
         [Required(ErrorMessage = "Title is required")]
-        [StringLength(200, MinimumLength = 5, ErrorMessage = "Title must be between 5 and 200 characters")]
+        [StringLength(200, MinimumLength = 5, ErrorMessage = "Title must be above 5 characters")]
         public string Title { get; set; } = null!;
 
         [Required(ErrorMessage = "Description is required")]
-        [StringLength(100, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 100 characters")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "Description must be above 10 characters")]
         public string Description { get; set; } = null!;
 
         [Required(ErrorMessage = "Category is required")]
